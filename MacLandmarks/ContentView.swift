beb104a85602f759db5fc8ffb4f2d81ec536af01
marchesio <sdusunlin@gmail.com>
2020-12-25 11:02:15 -0800

    MacOS app 1

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by lin sun on 12/25/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
