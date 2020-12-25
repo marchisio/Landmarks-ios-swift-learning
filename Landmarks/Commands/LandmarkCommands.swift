079d4fd947ffcc8e4dfb78b2054d9db191435afa
marchesio <sdusunlin@gmail.com>
2020-12-25 14:58:48 -0800

    macOS App 5

---
+++ Landmarks/Commands/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by lin sun on 12/25/20.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
